f38196b79514b9ef1f1019fe269872766b8369e6
althafdemiandra <100656575+althafdemiandra@users.noreply.github.com>
2025-02-08 17:57:22 +0700

    a

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { HELOO } from './something/stuff.js'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { HELOO } from "./something/stuff.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-    console.log(HELOO)
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	console.log(HELOO);
+	return c.json({
+		hello: "world",
+	});
+});
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
